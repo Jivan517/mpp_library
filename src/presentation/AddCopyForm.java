0e3fb86a87ff8f9ea9bb02d7670f295931b1a83e
peng <neildong@gmail.com>
2016-03-01 11:13:32 -0600

    add addcopyfomr

---
+++ src/presentation/AddCopyForm.java
@@ -0,0 +1,23 @@
+package presentation;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AddCopyForm extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("AddCopyForm.fxml"));
+		Scene scene = new Scene(root, 500, 400);
+		primaryStage.setTitle("Add Library Member");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+
+}
