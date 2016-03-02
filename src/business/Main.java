f139c88c66875fe908771990ba5cabe398eecb61
peng <neildong@gmail.com>
2016-03-02 14:13:14 -0600

    book

--- src/business/Main.java
+++ src/business/Main.java
@@ -25,31 +25,28 @@ public class Main extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		LibraryMember member = new LibraryMember("3434", "John4");
-
-		ArrayList<Author> authors = new ArrayList();
-		authors.add(new Author("Misss", "Guuu", "crreeee", "TOTOTOTTO"));
-
-		Book b = new Book("COBY", "0913", authors);
-		Publication p = b;
-
-		LendableCopy c = new LendableCopy(343, p);
-		c.setPublication(p);
-		c.setCopyId(1);
+//		LibraryMember member = new LibraryMember("3434", "John4");
+//
+//		ArrayList<Author> authors = new ArrayList();
+//		authors.add(new Author("Misss", "Guuu", "crreeee", "TOTOTOTTO"));
+//
+//		Book b = new Book("COBY", "0913", authors);
+//		Publication p = b;
+//
+//		LendableCopy c = new LendableCopy(343, p);
+//		c.setPublication(p);
+//		c.setCopyId(1);
 		//member.checkout(c, LocalDate.now(), LocalDate.now().plus(30, ChronoUnit.DAYS));
 
-		DataAccessFacade accessFacade = new DataAccessFacade();
+//		DataAccessFacade accessFacade = new DataAccessFacade();
 //		accessFacade.saveBook("bukk", b);
 
 		//accessFacade.saveLendableCopy(4455, c);
-		System.out.println(accessFacade.readLendableCopy(4455));
+//		System.out.println(accessFacade.readLendableCopy(4455));
 
 		//LibraryMember m = accessFacade.readLibraryMember("john.ser");
 		//System.out.println(m.toString());
 
-
-//		LoginForm lf = new LoginForm();
-//		lf.start(primaryStage);
 
 		LoginForm lf = new LoginForm();
 		lf.start(primaryStage);
--- src/presentation/AddCopyForm.java
+++ src/presentation/AddCopyForm.java
@@ -1,15 +1,46 @@
 package presentation;
 
+import business.Book;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class AddCopyForm extends Application {
+	private Book book = null;
+	@FXML private TextField isbnText;
+	@FXML private TextField tcid;
+	@FXML private Label isbn, title,  copynum, author, popular;
+	@FXML private Label bid;
+	@FXML protected void handleAddButtonAction(ActionEvent event) {
+		if(book == null){
+			System.out.println("No book selected");
+			return;
+		}
+		if(tcid.getText().length() <= 0 || Integer.parseInt(tcid.getText()) <= 0){
+			System.out.println("You should enter the correct copy id");
+		}
+	}
+	@FXML protected void handleSearchButtonAction(ActionEvent event) {
 
+		book = Book.bookWithISBN(this.isbnText.getText());
+		if(book == null){
+			System.out.println("No such book");
+			return;
+		}
+		System.out.println("read one book");
+		isbn.setText(book.getISBN());
+		title.setText(book.getTitle());
+		bid.setText(Integer.toString(book.getId()));
+		copynum.setText(Integer.toString(book.numberOfCopies()));
+	}
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("AddCopyForm.fxml"));
@@ -20,4 +51,5 @@ public class AddCopyForm extends Application {
 		
 	}
 
+	
 }
