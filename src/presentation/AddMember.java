71aded27a923bf82a9c63df72d886f6153242e0b
Jivan 'Zane <Jivan 'Zane>
2016-03-02 14:08:29 -0600

    add libraryMember

--- src/presentation/AddMember.java
+++ src/presentation/AddMember.java
@@ -2,6 +2,9 @@ package presentation;
 
 import java.awt.TextField;
 
+import business.AdminRole;
+import business.LibraryMember;
+import business.SystemUser;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,7 +16,13 @@ import javafx.stage.Stage;
 public class AddMember extends Application {
 
 	private TextField txtmember_id;
-	
+	private TextField txtFirstName;
+	private TextField txtLastName;
+	private TextField txtStreet;
+	private TextField txtCity;
+	private TextField txtState;
+	private TextField txtZip;
+	private TextField txtPhone;	
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -26,11 +35,35 @@ public class AddMember extends Application {
 
 	@FXML protected void handleSubmitButtonAction(ActionEvent event) throws Exception {
 
-		System.out.println(this.txtmember_id.getText());
+		String memberId = txtmember_id.getText();
+		String firstName = txtFirstName.getText();
+		String lastName  = txtLastName.getText();
+		String street = txtStreet.getText();
+		String state = txtState.getText();
+		String city = txtCity.getText();
+		String zipCode = txtZip.getText();
+		String phone = txtPhone.getText();
+		
+		LibraryMember member = new LibraryMember(memberId, firstName + " " + lastName);
+		
+		//This user will be the logged in user
+		SystemUser user = new SystemUser();
+		if(user.isAdmin()){
+			AdminRole admin = new AdminRole();
+			admin.addMember(member);
+		}
 	   
 	}
 	@FXML protected void handleResetButtonAction(ActionEvent event) throws Exception {
 	       
-		   
+		txtmember_id.setText("");
+		txtFirstName.setText("");
+		txtLastName.setText("");
+		txtStreet.setText("");
+		txtState.setText("");
+		txtCity.setText("");
+		txtZip.setText("");
+		txtPhone.setText("");
+		
 	}
 }
