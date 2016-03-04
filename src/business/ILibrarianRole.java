ae5f08fee85de97afc829a15a9089ab097bffbcc
zeelosoft <zeelosoft@gmail.com>
2016-03-04 10:49:21 -0600

    done the checkout

--- src/business/ILibrarianRole.java
+++
@@ -1,8 +0,0 @@
-package business;
-
-import java.time.LocalDate;
-
-public interface ILibrarianRole extends IRole {
-public boolean checkOut(LibraryMember member,LendableCopy copy,
-		LocalDate dueDate,LocalDate checkoutDate);
-}
